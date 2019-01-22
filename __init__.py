426462dbefee4f04040918e8d36cb04881a20832
Ivan 이반 <is3430@mpaycheck.com>
2019-01-22 13:08:34 +0900

    add confirmation alert message and method to manually input days

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 #  Ivan Schurawel
 # October 21, 2018
+# January 22, 2019
 
 """
 This program is free software: you can redistribute it and/or modify
@@ -21,12 +22,14 @@ from aqt import mw
 from aqt.qt import *
 
 from anki.cards import Card
-from aqt.utils import askUser, tooltip
+from aqt.utils import askUser, getOnlyText, showWarning, tooltip
 from anki.hooks import addHook
 
 import time
 import datetime
 
+DUE = 2
+SUSPENDED = -1
 
 def onDeckBrowserDelayCards(did):
     deck_name = mw.col.decks.name(did)
@@ -34,40 +37,65 @@ def onDeckBrowserDelayCards(did):
     if not cids:
         tooltip("Deck contains no cards.")
         return
-    r = askUser("This will delay <b>ALL</b> cards in the deck"
+    ask_response = askUser("This will delay <b>ALL</b> cards in the deck"
                 " '{}' by the number of days the deck is overdue."
                 "<br><br>Are you sure you want to proceed?".format(deck_name),
                 defaultno=True, title="Delay Overdue Cards")
-    if not r:
+    if not ask_response:
         return
-    delay_cards(did)
 
-
-def delay_cards(did):
     deckManager = mw.col.decks
+    cards = [Card(mw.col, cid) for cid in deckManager.cids(did)]
+
+    days_to_delay = calculate_delay(did, cards)
+
+    confirm_response = askUser("The oldest card is <b>{}</b> days overdue."
+                " Delay all cards by {} days?"
+                "<br><br>If you press 'NO' you will be able to manually enter"
+                " how many days to delay by.".format(days_to_delay, days_to_delay),
+                defaultno=True, title="Confirm Delay")
+                
+    if not confirm_response:
+        days_to_delay = getOnlyText("How many days would you like to delay? (Negative numbers will bring days forward)")
+
+        if days_to_delay is "":
+            return
+        try:
+            days_to_delay = int(days_to_delay)
+            if type(days_to_delay) != int:
+                raise ValueError('Not a valid int')
+                return
+        except:
+                showWarning("Please only enter interger numbers")
+                return
+
+    delay_cards(did, deckManager, cards, days_to_delay)
+
+def calculate_delay(did, cards):
     today_date = datetime.date.today()
     collection_creation = mw.col.crt
     collection_creation_date = datetime.date.fromtimestamp(collection_creation)
     difference = (today_date - collection_creation_date).days
-    cards = [Card(mw.col, cid) for cid in deckManager.cids(did)]
     max_days_due = 0
-    DUE = 2
-    SUSPENDED = -1
     for card in cards:
         if card.type == DUE and card.queue != SUSPENDED and card.due <= difference:
             days_due = difference - card.due
             if days_due > max_days_due:
                 max_days_due = days_due
+
+    return max_days_due
+
+def delay_cards(did, deckManager, cards, days_to_delay):
     for card in cards:
-        if card.type == DUE and card.queue != SUSPENDED:
-            adjusted_due_date = card.due + max_days_due
+        if card.type == DUE:
+            adjusted_due_date = card.due + days_to_delay
             card.due = adjusted_due_date
             card.flush()
     deck = deckManager.get(did)
     mw.col.decks.save(deck)
     mw.col.decks.flush()
     mw.deckBrowser.refresh()
-    tooltip("Delayed deck by: %d days" % max_days_due)
+    tooltip("Delayed deck by: %d days" % days_to_delay)
 
 
 def onDeckBrowserShowOptions(menu, did):
